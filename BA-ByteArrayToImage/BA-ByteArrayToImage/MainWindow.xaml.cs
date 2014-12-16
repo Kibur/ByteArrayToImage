6565ec6488230a1321057ff7334dd60b8482eb57
Kibur <manu2293@hotmail.com>
2014-12-16 19:27:52 +0100

    Project

---
+++ BA-ByteArrayToImage/BA-ByteArrayToImage/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BA_ByteArrayToImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private BitmapImage BitmapImageFromBytes(byte[] bytes)
+        {
+            BitmapImage image = null;
+            MemoryStream stream = null;
+
+            try
+            {
+                stream = new MemoryStream(bytes);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                System.Drawing.Image img = System.Drawing.Image.FromStream(stream);
+
+                image = new BitmapImage();
+                image.BeginInit();
+
+                MemoryStream ms = new MemoryStream();
+
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                image.StreamSource = ms;
+                image.StreamSource.Seek(0, SeekOrigin.Begin);
+                image.EndInit();
+            }
+            catch (Exception)
+            {
+                // Just don't crash please
+            }
+            finally
+            {
+                stream.Close();
+                stream.Dispose();
+            }
+
+            return image;
+        }
+
+        #region hex string to corresponding byte array
+        public static byte[] ConvertToByteArray(string value)
+        {
+            byte[] bytes = null;
+            if (String.IsNullOrEmpty(value))
+                bytes = null;
+            else
+            {
+                int string_length = value.Length;
+                int character_index = (value.StartsWith("0x", StringComparison.Ordinal)) ? 2 : 0; // Does the string define leading HEX indicator '0x'. Adjust starting index accordingly.               
+                int number_of_characters = string_length - character_index;
+
+                bool add_leading_zero = false;
+                if (0 != (number_of_characters % 2))
+                {
+                    add_leading_zero = true;
+
+                    number_of_characters += 1;  // Leading '0' has been striped from the string presentation.
+                }
+
+                bytes = new byte[number_of_characters / 2]; // Initialize our byte array to hold the converted string.
+
+                int write_index = 0;
+                if (add_leading_zero)
+                {
+                    bytes[write_index++] = FromCharacterToByte(value[character_index], character_index);
+                    character_index += 1;
+                }
+
+                for (int read_index = character_index; read_index < value.Length; read_index += 2)
+                {
+                    byte upper = FromCharacterToByte(value[read_index], read_index, 4);
+                    byte lower = FromCharacterToByte(value[read_index + 1], read_index + 1);
+
+                    bytes[write_index++] = (byte)(upper | lower);
+                }
+            }
+
+            return bytes;
+        }
+
+        private static byte FromCharacterToByte(char character, int index, int shift = 0)
+        {
+            byte value = (byte)character;
+            if (((0x40 < value) && (0x47 > value)) || ((0x60 < value) && (0x67 > value)))
+            {
+                if (0x40 == (0x40 & value))
+                {
+                    if (0x20 == (0x20 & value))
+                        value = (byte)(((value + 0xA) - 0x61) << shift);
+                    else
+                        value = (byte)(((value + 0xA) - 0x41) << shift);
+                }
+            }
+            else if ((0x29 < value) && (0x40 > value))
+                value = (byte)((value - 0x30) << shift);
+            else
+                throw new InvalidOperationException(String.Format("Character '{0}' at index '{1}' is not valid alphanumeric character.", character, index));
+
+            return value;
+        }
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] photo = ConvertToByteArray(txtInput.Text);
+
+            imgOutput.Source = BitmapImageFromBytes(photo);
+
+            int i = 0;
+        }
+    }
+}
